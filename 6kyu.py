7e9642e0e75e50b7665c51b7c87d5adff7990ce4
Timothy <Timothy.viccari@gmail.com>
2021-08-18 18:45:08 -0400

    cadd multiples of 3 or 5

--- 6kyu.py
+++ 6kyu.py
@@ -1,3 +1,6 @@
+
+###-----------VALID BRACES-----------###
+
 # Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
 
 # This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
@@ -36,3 +39,20 @@ def validBraces(string):
     if stack:
         return False
     return True
+
+    
+###-----------Multiples of 3 or 5-----------###
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+# Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(number):
+    solution_list = []
+    for i in range(number):
+        if i % 3 == 0 or i % 5 == 0:
+            solution_list.append(i)
+    answer = sum(solution_list)
+    return answer
